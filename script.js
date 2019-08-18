f700beac7b6754db3d6eebb6e6563f46b04da662
Inga Szeliga <inga.szeliga@gmail.com>
2019-08-18 11:46:48 +0200

    Debugged button Start

--- script.js
+++ script.js
@@ -1,50 +1,54 @@
-const bowls = document.querySelectorAll('.bowl');
-const scoreBoard = document.querySelector('.score');
-const avocados = document.querySelectorAll('.avocado');
+const bowls = document.querySelectorAll(".bowl");
+const scoreBoard = document.querySelector(".score");
+const avocados = document.querySelectorAll(".avocado");
 //Save the last bowl to prevent the same bowl 2 times in a row
 let lastBowl;
 let timeUp = false;
 let score = 0;
+let isGameStarted = false;
 
 function randomTime(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
+  return Math.round(Math.random() * (max - min) + min);
 }
 
 function randomBowl(bowls) {
-    const index = Math.floor(Math.random() * bowls.length);
-    const bowl = bowls[index];
-    if (lastBowl === bowl) {
-        return randomBowl(bowls)
-    }
+  const index = Math.floor(Math.random() * bowls.length);
+  const bowl = bowls[index];
+  if (lastBowl === bowl) {
+    return randomBowl(bowls);
+  }
 
-    lastBowl = bowl;
+  lastBowl = bowl;
 
-    return bowl;
+  return bowl;
 }
 
 function popUp() {
-    const time = randomTime(400, 1000);
-    const bowl = randomBowl(bowls);
-    bowl.classList.add('up');
-    setTimeout(() => {
-        bowl.classList.remove('up');
-        if (!timeUp) popUp();
-    }, time)
+  const time = randomTime(400, 1000);
+  const bowl = randomBowl(bowls);
+  bowl.classList.add("up");
+  setTimeout(() => {
+    bowl.classList.remove("up");
+    if (!timeUp) popUp();
+  }, time);
 }
 
 function startGame() {
+  if (isGameStarted === false) {
+    isGameStarted = true;
     scoreBoard.textContent = 0;
     timeUp = false;
     score = 0;
     popUp();
-    setTimeout(() => timeUp = true, 30000);
+    setTimeout(() => (timeUp = true), 30000);
+  }
 }
 
 function hit(event) {
-    if(!event.isTrusted) return;
-    score++;
-    this.classList.remove('up');
-    scoreBoard.textContent = score;
+  if (!event.isTrusted) return;
+  score++;
+  this.classList.remove("up");
+  scoreBoard.textContent = score;
 }
 
-avocados.forEach((avocado) => avocado.addEventListener('click', hit));
+avocados.forEach(avocado => avocado.addEventListener("click", hit));
